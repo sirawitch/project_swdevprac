a50d811f1cc3ffb799537ecb0764383241a1b0e2
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2025-10-23 23:34:19 +0700

    Update Config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,11 +4,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**", // อนุญาตทุก Hostname ที่ใช้ HTTPS (วิธีที่ง่ายแต่ปลอดภัยน้อยกว่า)
+        protocol: 'https',
+        hostname: '**', // อนุญาตทุก Hostname ที่ใช้ HTTPS (วิธีที่ง่ายแต่ปลอดภัยน้อยกว่า)
         // หรือถ้าคุณรู้ Domain ที่แน่นอน: hostname: 'storage.googleapis.com',
       },
+    
     ],
+    domains:[
+      'drive.google.com'
+    ]
     // หรือใช้วิธี domains แบบเดิม (ง่ายกว่าสำหรับ Next.js v13/14)
     // domains: ['images.unsplash.com', 'your-image-cdn.com', 'i.imgur.com'],
   },
